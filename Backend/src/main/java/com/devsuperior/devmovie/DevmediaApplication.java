e8f2d16cb32ba748df02e77b86a7b1d9777e9b8a
tiago.ferreira <tiago.ferreira@crptecnologia.com.br>
2022-03-08 15:40:15 -0300

    projeto criado

---
+++ Backend/src/main/java/com/devsuperior/devmovie/DevmediaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevmediaApplication.class, args);
+    }
+
+}
